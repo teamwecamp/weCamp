8a12f8e8d086043fb31f022f7a979bc0ed6b83b7
Tang Yang <ty4081097@gmail.com>
2019-03-08 10:13:47 -0600

    add new files to project.

---
+++ prime-group-project-master /src/components/Landing/LandingRecentCamps.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+
+class LandingRecentCamps extends Component {
+    render() {
+        return (
+            <div>Landing Recent Camps</div>
+        )
+    }
+}
+
+export default LandingRecentCamps;
---
+++ prime-group-project-master /src/components/Landing/LandingSponsoredCamps.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+
+class LandingSponsoredCamps extends Component {
+    render() {
+        return (
+            <div>Landing Sponsored Camps</div>
+        )
+    }
+}
+
+export default LandingSponsoredCamps;
