24a1d1dbf2cda42cfdb32f368daa34bc1f87e5cd
Nick Neiert <nickster7421@gmail.com>
2019-03-11 11:40:39 -0500

    added the base searchRouter and also aded it to the serverJS

--- prime-group-project-master /server/server.js
+++ prime-group-project-master /server/server.js
@@ -12,6 +12,7 @@ const passport = require('./strategies/user.strategy');
 const userRouter = require('./routes/user.router');
 const landingRouter = require('./routes/landing.router');
 const favoriteRouter = require('./routes/favorite.router');
+const searchRouter = require('./routes/search.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -28,6 +29,7 @@ app.use(passport.session());
 app.use('/api/user', userRouter);
 app.use('/api/landing', landingRouter);
 app.use('/api/favorite',favoriteRouter);
+app.use('/api/search', searchRouter);
 
 // Serve static files
 app.use(express.static('build'));
