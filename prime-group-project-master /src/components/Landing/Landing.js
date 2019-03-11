f2e1a2a0769391c240ab58f8e90ee55eafd91f3d
Erin Olson <erineolson15@gmail.com>
2019-03-11 17:19:34 -0500

    updated previous updates

--- prime-group-project-master /src/components/Landing/Landing.js
+++ prime-group-project-master /src/components/Landing/Landing.js
@@ -13,7 +13,7 @@ goToSearch = ()=>{
         return(
             <div>
                 <div>Landing</div>
-                {JSON.stringify(this.props.reduxStore.LandingReducer.campName)}
+                {JSON.stringify(this.props.reduxStore.LandingReducer)}
                 <button onClick={this.goToSearch}>
                 Find Camps
                 </button>
