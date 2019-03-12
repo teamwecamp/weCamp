581e5d03409167c6d41df909d71ea8ae174303f2
Britt Carter <brittbmh@gmail.com>
2019-03-12 16:58:25 -0500

    add grid to hold favorite camps

--- prime-group-project-master /src/components/App/App.js
+++ prime-group-project-master /src/components/App/App.js
@@ -18,6 +18,7 @@ import UserPage from '../UserPage/UserPage';
 import InfoPage from '../InfoPage/InfoPage';
 import Landing from '../Landing/Landing';
 import SearchCamps from '../Search/SearchCamps';
+import Favorites from '../Favorite/FavoriteCamps';
 
 import './App.css';
 
@@ -68,6 +69,11 @@ class App extends Component {
               exact
               path="/info"
               component={InfoPage}
+            />
+            <ProtectedRoute
+              exact
+              path="/favorites"
+              component={Favorites}
             />
             {/* If none of the other routes matched, we will show a 404. */}
             <Route render={() => <h1>404</h1>} />
--- prime-group-project-master /src/components/Favorite/FavoriteCamps.js
+++ prime-group-project-master /src/components/Favorite/FavoriteCamps.js
@@ -1,32 +1,66 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Grid from '@material-ui/core/Grid';
 
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    control: {
+        padding: theme.spacing.unit * 2,
+    },
+    pos: {
+        marginBottom: 12,
+    },
+});
 
 class FavoriteCamps extends Component {
 
     componentDidMount(){
-        this.setFavoriteCamps();
+        // this.setFavoriteCamps();
     }
 
     setFavoriteCamps = () => {
         const action = { type: 'SET_FAVORITE_CAMPS'}
         this.props.dispatch(action);
-        console.log('action', action);
-        
+    }
+
+    favoriteCamps = () => {
+        if (this.props.favoriteCamps.length === 0) {
+            return (
+                <div>
+                    <p>You have not selected any favorites.</p>
+                </div>
+            )
+        } else {
+            return (
+                this.props.userHostEvents.map((hostEvent, i) => {
+                    return (<FavoriteCampItems moveToEvent={this.moveToEvent} key={i} event={hostEvent} />)
+                })
+            )
+        }
     }
 
 
     render() {
         return (
-            <div>Favorite Camps
-                {JSON.stringify(this.props.reduxStore.setFavoriteCamps)}
+            <div>
+                <h1>Favorite Camps</h1>
+                {/* {JSON.stringify(this.props.reduxStore.setFavoriteCamps)} */}
+                <Grid container className={classes.root} justify="center" spacing={Number(spacing)}>
+                    {this.favoriteCamps()}
+                </Grid>
             </div>
         )
     }
 }
 
 const mapStateToProps = (reduxStore) => ({
-    reduxStore
+    favoriteCamps: reduxStore.setFavoriteCamps
 });
 
-export default connect(mapStateToProps)(FavoriteCamps);
+Landing.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(connect(mapStateToProps)(FavoriteCamps));
