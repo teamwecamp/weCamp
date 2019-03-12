1dcedd0e84af3c6bc63bbaaf122944929b3aacbf
Britt Carter <brittbmh@gmail.com>
2019-03-12 13:43:45 -0500

    add fields to the registration  page

--- prime-group-project-master /src/components/UserRegistration/UserRegistration.js
+++ prime-group-project-master /src/components/UserRegistration/UserRegistration.js
@@ -8,6 +8,7 @@ class UserRegistration extends Component {
     render() {
         return (
             <div>User Registration</div>
+            //I think this already exists as the RegisterPage
         )
     }
 }
