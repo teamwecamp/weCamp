56b5f3c71656bdd0944f25a13902d6c08b2c15dd
Erin Olson <erineolson15@gmail.com>
2019-03-13 10:19:44 -0500

    created get route for favorites-functions as expected- returns an array of objects which contain user cild name with favorite camp and selected camp info

--- prime-group-project-master /server/routes/favorite.router.js
+++ prime-group-project-master /server/routes/favorite.router.js
@@ -2,19 +2,6 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-///this SQL statment will need to change
-// router.get('/', (req, res) => {
-//     console.log('this is inside router get Favorite..');
-//     const queryText = `SELECT * FROM "camp"`;
-//     pool.query(queryText)
-//     .then(result => {
-//         res.send(result.rows);
-//     }).catch(error => {
-//         console.log('there is error in get favorite router', error);
-//         res.sendStatus(500);
-//     })
-
-// })
 
 router.get('/', (req, res) => {
     if(req.isAuthenticated()){
@@ -25,28 +12,48 @@ router.get('/', (req, res) => {
         try {
             await client.query('BEGIN');
             
+            //user.id is logged in user
             const user = req.user.id;
-            let queryText = `SELECT "favorites"."camp_id", "favorites"."user_kid_id"
-                                FROM "favorites"
-                                JOIN "user_child"
-                                ON  "favorites"."user_kid_id"= "user_child"."child_id"
-                                WHERE "user_child"."user_id" = $1`;
+            //selecting favorite camp id and child name - joinign favorites to user child and child 
+            //porfile to user table
+            let queryText = `SELECT "favorites"."camp_id", "child_profile"."name"
+                    FROM "favorites" 
+                    JOIN "user_child" 
+                    ON "favorites"."user_kid_id"="user_child"."child_id"
+                    JOIN "child_profile"
+                    ON "user_child"."child_id"="child_profile"."id"
+                    WHERE "user_child"."user_id"= $1`;
             const favoriteList = await client.query(queryText,[user]);
+            //turns favorite into an array
             const favorites = favoriteList.rows;
+            
+            console.log('favoritelistr', favorites);
+            
             console.log('favorite list row', favorites);
-            let answer = [];
+            //create empty array to push data into
+            let favCamps =[];
             for (let favorite of favorites) {
-                queryText = `SELECT "camp"."name", "camp"."photo_url", "camp"."website" 
+                let camp = favorite.camp_id;
+                let kid = favorite.name;
+                console.log('kid', kid);
+                console.log('camp', camp);
+                
+                //selecting camp name & info based on id received from above
+                queryText = `SELECT "camp"."Name", "camp"."photo_url", "camp"."website"
                                 FROM "camp" 
                                 WHERE "camp"."id" =$1`;
                 const secondPull = await client.query(queryText,[favorite.camp_id]);
                 let result = secondPull.rows[0];
-                console.log(result);
-                result.campId = favorite.campId;
-                answer.push(result);
+                //create an empty object for data
+                let faves = {};
+                //to push into object
+                faves.camp= result;
+                faves.kid=kid;
+                // faves gets pshed into empty array from above
+                favCamps.push(faves);
             }
             await client.query('COMMIT');
-            res.send(answer);
+            res.send(favCamps);
         } catch (error) {
             console.log('Rollback', error);
             await client.query('ROLLBACK');
