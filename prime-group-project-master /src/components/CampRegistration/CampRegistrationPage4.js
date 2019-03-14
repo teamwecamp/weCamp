cba222beaaed3e86ea28d5af7e66d4750580036e
Nick Neiert <nickster7421@gmail.com>
2019-03-14 13:56:08 -0500

    added functions to dynamically add state to programs array

--- prime-group-project-master /src/components/CampRegistration/CampRegistrationPage4.js
+++ prime-group-project-master /src/components/CampRegistration/CampRegistrationPage4.js
@@ -31,17 +31,19 @@ const styles = theme => ({
 
 class CampRegistrationPage4 extends Component {
     state = {
-        campName:'',
-        campAddress:'',
-        state:'',
-        region:'',
-        startDate:'',
-        endDate:'',
-        minAge:'',
-        maxAge:'',
-        gender:'',
-        religion:'',
-        accessibility:'',
+        programs:[{
+            programTitle:'',
+            cost:'',
+            startTime:'',
+            endTime:'',
+            type:'',
+            age:'',
+            gender:'',
+            startDate:'',
+            endDate:'',
+        }],
+        
+        
     }
     componentDidMount = () => {
         //insert action to get drop down info here.
@@ -51,11 +53,57 @@ class CampRegistrationPage4 extends Component {
         const action = {type: 'FETCH_SEARCH_CAMPS_DROP_DOWN'};
         this.props.dispatch(action);
     }
+    handleAddMorePrograms = event => {
+        this.setState({
+            programs: [...this.state.programs, 
+                {
+                    programTitle:'',
+                    cost:'',
+                    startTime:'',
+                    endTime:'',
+                    type:'',
+                    age:'',
+                    gender:'',
+                    startDate:'',
+                    endDate:'',
+                }
+            ],
+        });
+    }
+    handleProgramChange = (index, key) => event => {
+        let programs = [...this.state.programs];
+        let program = {
+            ...programs[index],
+            [key]: event.target.value // e.g. latitude: 50 or longitude: 123
+        };
+        programs[index] = program;
+        this.setState({
+            programs: programs
+        });
+    }
 
     render() {
+        const { classes } = this.props;
         return (
             <div>
             <h1>Camp Registration Page 4</h1>
+            <form
+          id="input-form"
+          className={classes.container}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            required
+            id="outlined-icon"
+            label=" Cost Min"
+            className={classes.textField}
+            value={this.state.costMin}
+            onChange={this.handleInputChangeFor("costMin")}
+            margin="normal"
+            variant="standard"
+          />
+          </form>
             </div>
         )
     }
