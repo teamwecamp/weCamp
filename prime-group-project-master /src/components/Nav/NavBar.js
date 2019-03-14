f4548176cd288f5142b9abe869cac93430ccd3a4
Erin Olson <erineolson15@gmail.com>
2019-03-14 13:34:35 -0500

    updated nav bar, added drawer

---
+++ prime-group-project-master /src/components/Nav/NavBar.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Drawer from './Drawer';
+import LogOutButton from '../LogOutButton/LogOutButton';
+import './Nav.css';
+
+
+const NavBar = (props) => {
+    return (
+        <div >
+            <AppBar position="static">
+                <Toolbar className="nav">
+                    <Drawer />
+                    <Link to="/home">
+                        <h2 className="nav-title">WeCAMP</h2>
+                        {/* <NavBar/> */}
+                    </Link>
+                    <div className="nav-right">
+                        <Link className="nav-link" to="/home">
+                            {/* Show this link if they are logged in or not,
+        but call this link 'Home' if they are logged in,
+        and call this link 'Login / Register' if they are not */}
+                            {props.user.id ? 'Home' : 'Login / Register'}
+                        </Link>
+                        {/* Show the link to the info page and the logout button if the user is logged in */}
+                        {props.user.id && (
+                            <>
+                                <Link className="nav-link" to="/info">
+                                    Info Page
+          </Link>
+                                <LogOutButton className="nav-link" />
+                            </>
+                        )}
+                        {/* Always show this link since the about page is not protected */}
+                        <Link className="nav-link" to="/about">
+                            About
+      </Link>
+                    </div>
+ 
+                </Toolbar>
+            </AppBar>
+        </div>
+
+    );
+}
+
+const mapStateToProps = state => ({
+    user: state.user,
+});
+
+export default connect(mapStateToProps)(NavBar);
