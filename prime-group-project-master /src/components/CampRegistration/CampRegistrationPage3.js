00983400da696e328d6ae7e9aa63cbf622c1cae6
Nick Neiert <nickster7421@gmail.com>
2019-03-14 11:37:32 -0500

    added bind and state to component with a component did mount function

--- prime-group-project-master /src/components/CampRegistration/CampRegistrationPage3.js
+++ prime-group-project-master /src/components/CampRegistration/CampRegistrationPage3.js
@@ -30,19 +30,23 @@ const styles = theme => ({
 
 
 class CampRegistrationPage3 extends Component {
-    state = {
-        campName:'',
-        campAddress:'',
-        state:'',
-        region:'',
-        startDate:'',
-        endDate:'',
-        minAge:'',
-        maxAge:'',
-        gender:'',
-        religion:'',
-        accessibility:'',
+    constructor(props){
+        super(props);
+        this.state = {
+
+        };
+        this.handleChange = this.handleChange.bind(this);
     }
+    handleChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const activity = target.name;
+
+        this.setState({
+            [activity]: value
+        })
+    }
+    
     componentDidMount = () => {
         //insert action to get drop down info here.
         this.getDropDowns();
@@ -52,10 +56,27 @@ class CampRegistrationPage3 extends Component {
         this.props.dispatch(action);
     }
 
+    setOptions = (event) => {
+        event.preventDefault();
+        const trueState = [];
+        for (let key in this.state){
+            if (this.state[key] === true){
+                trueState.push(key);
+            }
+        }
+        console.log(trueState);
+        this.props.history.push('//campregistrationpage4')
+    }
+
     render() {
         return (
             <div>
             <h1>Camp Registration Page 3</h1>
+            <h2>Select Activities Provided by your camp</h2>
+            <form onSubmit={this.setOptions}>
+                    {this.mapInfo()}
+                    <button type="submit">Submit</button>
+                </form>
             </div>
         )
     }
@@ -66,7 +87,7 @@ CampRegistrationPage3.propTypes = {
 };
 
 const mapStateToProps = (reduxStore) => ({
-    reduxStore
+    dropDown: reduxStore.setSearchCamps.setCampDropDown
 });
 
 export default connect(mapStateToProps)(withStyles(styles)(CampRegistrationPage3));
