9cbf798aacc589b5d2407f4159888912700ba572
Tang Yang <ty4081097@gmail.com>
2019-03-14 11:37:30 -0500

    add get routes to get user info.

--- prime-group-project-master /server/routes/userprofileinfo.router.js
+++ prime-group-project-master /server/routes/userprofileinfo.router.js
@@ -8,8 +8,31 @@ const router = express.Router();
  * GET route template
  */
 router.get('/user', (req, res) => {
+    console.log('this is in gets user profile')
+    if(req.isAuthenticated){
+        //user.id is logged in user
+        const user = req.user.id;
+        const queryText = `SELECT * FROM "user"
+                       WHERE "user"."id" = $1;`;
+        pool.query(queryText, [user])
+        .then(result => {
+            res.send(result.rows);
+        }).catch(error => {
+            console.log('there is error in get user router', error);
+        })
+
+
+
+    }
+    
     
 });
+
+
+router.get('/child', (req, res)=> {
+    console.log('this is in gets child profile')
+
+})
 
 /**
  * POST route template
