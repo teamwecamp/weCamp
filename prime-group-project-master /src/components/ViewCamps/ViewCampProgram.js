e16c07fc1b516dfd725781f775d57fcba721a6cc
Tang Yang <ty4081097@gmail.com>
2019-03-15 16:48:12 -0500

    add material UI select to viewCampProgram.

--- prime-group-project-master /src/components/ViewCamps/ViewCampProgram.js
+++ prime-group-project-master /src/components/ViewCamps/ViewCampProgram.js
@@ -12,7 +12,11 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import DeleteOutlined from '@material-ui/icons/DeleteOutlined';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
 
 const CustomTableCell = withStyles(theme => ({
     head: {
@@ -30,7 +34,7 @@ const styles = theme => ({
         marginTop: theme.spacing.unit * 2,
         overflowX: 'auto',
         marginBottom: '10px',
-        marginLeft: '95px',
+        marginLeft: '100px',
     },
     table: {
         minWidth: 500,
@@ -53,16 +57,39 @@ class ViewCampProgram extends Component {
         this.getCampProgram();
     }
 
+    state = {
+        age: '',
+        open: false,
+    };
+
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
+
+    handleClose = () => {
+        this.setState({ open: false });
+    };
+
+    handleOpen = () => {
+        this.setState({ open: true });
+    };
+
     getCampProgram = () => {
         console.log('this is inside getCampProgram');
         const campId = this.props.match.params.id
         this.props.dispatch({ type: 'FETCH_CAMP_PROGRAMS', payload: campId });
     }
 
+    
+
+    
+       
+
     render(){
 
         console.log('this is camp program', this.props.campProgram );
         const { classes } = this.props;
+        // const { classes } = this.props;
         return(
           
             <Paper className={classes.root}>
@@ -99,7 +126,36 @@ class ViewCampProgram extends Component {
                         
                     </TableBody>
                 </Table>
+
+              {/* this is the form for selecting kids, program, and status */}
+                <form autoComplete="off">
+                    <FormControl className={classes.formControl}>
+                        <InputLabel htmlFor="demo-controlled-open-select">Add Itinerary To</InputLabel>
+                        <Select
+                            open={this.state.open}
+                            onClose={this.handleClose}
+                            onOpen={this.handleOpen}
+                            value={this.state.age}
+                            onChange={this.handleChange}
+                            inputProps={{
+                                name: 'age',
+                                id: 'demo-controlled-open-select',
+                            }}
+                        >
+                            <MenuItem value="">
+                                <em>Itinerary</em>
+                            </MenuItem>
+                            <MenuItem value={10}>Kids</MenuItem>
+                            <MenuItem value={20}>Program</MenuItem>
+                            <MenuItem value={30}>Status</MenuItem>
+                        </Select>
+                    </FormControl>
+                        
+                </form>
+
             </Paper>
+            
+           
            
         )
     }
