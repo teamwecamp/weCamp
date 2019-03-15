44a3cee1613a349f5a083fef855a717a1d2bb255
Britt Carter <brittbmh@gmail.com>
2019-03-15 15:38:01 -0500

    add itemRenderer to conditionally render items by status

--- prime-group-project-master /src/components/Itinerary/Itinerary.js
+++ prime-group-project-master /src/components/Itinerary/Itinerary.js
@@ -35,7 +35,7 @@ class Itinerary extends Component {
         this.setCampItinerary();
     }
 
-    getUserInfo(){
+    getUserInfo() {
         this.props.dispatch({ type: 'FETCH_USER_PROFILE_INFO', })
     }
 
@@ -44,15 +44,86 @@ class Itinerary extends Component {
         this.props.dispatch(action);
     }
 
+    statusColor = () => {
+        let status_id = this.props.itinerary.status_id;
+
+        if (status_id === 1) {
+            return 'rgb(124, 9, 163)';
+        } else if (status_id === 2) {
+            return 'rgb(234, 98, 114)';
+        } else if (status_id === 3) {
+            return 'rgb(124, 9, 163)';
+        } else if (status_id === 4) {
+            return 'rgb(124, 9, 163)';
+        };
+    }
+
+    itemRenderer = ({
+        item,
+        timelineContext,
+        itemContext,
+        getItemProps,
+        getResizeProps
+    }) => {
+        const { left: leftResizeProps, right: rightResizeProps } = getResizeProps();
+        const status = item.status_id
+        const backgroundColor = 
+
+        status === 1 ?
+             'rgb(124, 9, 163)' :
+        status === 2 ?
+            'rgb(234, 98, 114)' :
+        status === 3 ?
+            'rgb(124, 9, 163)' :
+        status === 4 ?
+            'rgb(124, 9, 163)': 'gray';
+        
+    
+        const borderColor = 'black';
+        return (
+            <div
+                {...getItemProps({
+                    style: {
+                        backgroundColor,
+                        color: item.color,
+                        borderColor,
+                        borderStyle: "solid",
+                        borderWidth: 1,
+                        borderRadius: 4,
+                        borderLeftWidth: itemContext.selected ? 3 : 1,
+                        borderRightWidth: itemContext.selected ? 3 : 1
+                    }
+                })}
+            >
+                {itemContext.useResizeHandle ? <div {...leftResizeProps} /> : null}
+
+                <div
+                    style={{
+                        height: itemContext.dimensions.height,
+                        overflow: "hidden",
+                        paddingLeft: 3,
+                        textOverflow: "ellipsis",
+                        whiteSpace: "nowrap"
+                    }}
+                >
+                    {itemContext.title}
+                </div>
+
+                {itemContext.useResizeHandle ? <div {...rightResizeProps} /> : null}
+            </div>
+        );
+    };
+
     render() {
         console.log(this.props.itinerary);
-        
+
+
         let items = this.props.itinerary.itineraries;
         let groups = this.props.itinerary.children;
         return (
             <div>
-                {this.props.itinerary.userName !== undefined && 
-                <h1>{this.props.itinerary.userName.full_name}'s Itinerary</h1>}
+                {this.props.itinerary.userName !== undefined &&
+                    <h1>{this.props.itinerary.userName.full_name}'s Itinerary</h1>}
                 {/* {JSON.stringify(this.props.itinerary)} */}
                 {this.props.itinerary.itineraries !== undefined &&
                     <Timeline
@@ -63,11 +134,12 @@ class Itinerary extends Component {
                         defaultTimeEnd="1567351065000"
                         canMove={false}
                         canResize={false}
+                        itemRenderer={this.itemRenderer}
                         lineHeight={50}
                         itemHeightRatio={.75}
                         sidebarContent={<div>Itinerary</div>}
                     />}
-                    <p>To zoom the calendar view out, click on the red bar in the header. To zoom in, click on the date detail bar.</p>
+                <p>To zoom the calendar view out, click on the red bar in the header. To zoom in, click on the date detail bar.</p>
             </div>
 
         )
