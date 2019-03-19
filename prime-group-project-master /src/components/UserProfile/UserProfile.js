24d96d1ce0aea455c527a7aea0ffd09b0ebf3cfb
Tang Yang <ty4081097@gmail.com>
2019-03-19 06:55:10 -0500

    add cards to project.

--- prime-group-project-master /src/components/UserProfile/UserProfile.js
+++ prime-group-project-master /src/components/UserProfile/UserProfile.js
@@ -1,5 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
+
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    control: {
+        padding: theme.spacing.unit * 2,
+    },
+    pos: {
+        marginBottom: 12,
+    },
+});
+
 
 
 
