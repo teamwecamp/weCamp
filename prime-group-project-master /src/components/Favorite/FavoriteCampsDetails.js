29dfa39c533435961afc4f70642f218931d91134
Tang Yang <ty4081097@gmail.com>
2019-03-19 08:32:32 -0500

    add cards to file.

--- prime-group-project-master /src/components/Favorite/FavoriteCampsDetails.js
+++ prime-group-project-master /src/components/Favorite/FavoriteCampsDetails.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
--- prime-group-project-master /src/components/UserProfile/UserProfile.js
+++ prime-group-project-master /src/components/UserProfile/UserProfile.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+
+// material UI cards
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 
-const styles = theme => ({
-    root: {
-        flexGrow: 1,
+
+const styles = {
+    card: {
+        maxWidth: 100,
     },
-    control: {
-        padding: theme.spacing.unit * 2,
+    media: {
+        height: 100,
     },
-    pos: {
-        marginBottom: 12,
-    },
-});
+};
 
 
 
@@ -41,26 +47,62 @@ class UserProfile extends Component {
         this.props.dispatch(action)
     }
 
+     
+
     render() {
+        // { JSON.stringify(this.props.user) }
+        // { JSON.stringify(this.props.child) }
         
-
+      
         return (
-            <div>
-                {JSON.stringify(this.props.user)}
-                {JSON.stringify(this.props.child)}
            
-
             
-            </div>
-        )
-    }
+
+            <Card  >
+                <CardActionArea>
+                    <CardMedia
+                        // className={classes.media}
+                        image="/static/images/cards/contemplative-reptile.jpg"
+                        title="Contemplative Reptile"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            Lizard
+                    </Typography>
+                        <Typography component="p">
+                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                            across all continents except Antarctica
+          </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button size="small" color="primary">
+                        Share
+        </Button>
+                    <Button size="small" color="primary">
+                        Learn More
+        </Button>
+                </CardActions>
+            </Card>
+        
+        
+  );
 }
+}
+
+        
+    
+
 
 const mapStateToProps = (reduxStore) => ({
     user: reduxStore.setUserProfileInfo,
     child: reduxStore.setChildProfileInfo
 });
 
+// MediaCard.propTypes = {
+//     classes: PropTypes.object.isRequired,
+// };
+
 
     
-    export default   connect(mapStateToProps)(UserProfile);
+export default  connect(mapStateToProps)(UserProfile);
