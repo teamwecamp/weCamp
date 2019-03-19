d2531817f9d357aadb772f3f86c180da3b7437cc
Britt Carter <brittbmh@gmail.com>
2019-03-19 10:17:42 -0500

    add popup with child select to share button

--- prime-group-project-master /src/components/Itinerary/ItineraryShare.js
+++ prime-group-project-master /src/components/Itinerary/ItineraryShare.js
@@ -1,14 +1,48 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Button from '@material-ui/core/Button';
+import Swal from 'sweetalert2';
 
 class ItineraryShare extends Component {
     shareItinerary = () => {
         console.log(this.props.children);
-        
+
+        Swal.fire({
+            title: 'Choose whose itinerary you want to share.',
+            input: 'select',
+            inputOptions: this.selectOptions(),
+            inputPlaceholder: 'Select a child',
+            showCancelButton: true,
+            inputValidator: (value) => {
+                if (!value) {
+                    return 'Please choose a child'
+                }
+            }
+        }).then((assignTo) => {
+            console.log(assignTo);
+            if (assignTo.value) {
+                const type = 'ADD_SHARE';
+                const payload = { child: assignTo.value, camp: this.props.camp.id };
+                this.props.dispatch({ type: type, payload: payload })
+            }
+        })
+
     }
-    render(){
-        return(
+
+    selectOptions = () => {
+        let children = this.props.children;
+        console.log(children);
+        let kids = {}
+        for (let child of children) {
+            let kid = child.id
+            kids[kid] = child.title
+        }
+        console.log(kids);
+        return kids;
+    }
+
+    render() {
+        return (
             <Button variant="contained" color="primary" size="large" onClick={this.shareItinerary}>
                 Share Itinerary
                 </Button>
