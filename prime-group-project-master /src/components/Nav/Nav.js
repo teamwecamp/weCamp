b7c9e230e52106888625809e888ef41097a48a03
Erin Olson <erineolson15@gmail.com>
2019-03-22 00:10:22 -0500

    updated login feature in navbar

--- prime-group-project-master /src/components/Nav/Nav.js
+++ prime-group-project-master /src/components/Nav/Nav.js
@@ -26,7 +26,7 @@ const Nav = (props) => (
       {/* Show the link to the info page and the logout button if the user is logged in */}
       {props.user.id && (
         <>
-          <Link className="nav-link" to="/">
+          <Link className="nav-link" to="/landing">
             Info Page
           </Link>
           <LogOutButton className="nav-link"/>
