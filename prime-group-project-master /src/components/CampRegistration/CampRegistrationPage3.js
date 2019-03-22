dccbbc684072fbcb268a4a7efd1320df40af6539
Erin Olson <erineolson15@gmail.com>
2019-03-22 00:28:31 -0500

    updated logout button on navbar

--- prime-group-project-master /src/components/CampRegistration/CampRegistrationPage3.js
+++ prime-group-project-master /src/components/CampRegistration/CampRegistrationPage3.js
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 import CampStepper from './CampStepper';
 import { withStyles, MuiThemeProvider } from '@material-ui/core/styles';
 import { createMuiTheme } from '@material-ui/core/styles';
+import Button from "@material-ui/core/Button";
 
 
 //this addes styling to only the form.
@@ -111,7 +112,12 @@ class CampRegistrationPage3 extends Component {
         <h2>Select Activities Provided by your camp</h2>
         <form onSubmit={this.setOptions}>
           {this.mapInfo()}
-          <button type="submit">Next</button>
+          <br></br>
+          <Button 
+          type="submit"
+          color="primary"
+          variant="contained"
+          size="small">Next</Button>
         </form>
         <CampStepper step = {this.state.activeStep}/>
         </MuiThemeProvider>
